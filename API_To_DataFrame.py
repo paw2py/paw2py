6a4bf86047f56c925fcb54367da726cd07aa6381
paw2py <pawanrudgi@gmail.com>
2021-02-03 15:41:28 -0500

    added code for getting column length sort and to show top 2 records

--- API_To_DataFrame.py
+++ API_To_DataFrame.py
@@ -6,5 +6,9 @@ import requests
 r = requests.get('rest api url')
 x = r.json()
 df = pd.DataFrame(x['data'])
-#
-print(df.head(5))
+df['com_len'] = df['title'].apply(len) #length of each titile in the data frame added a new column to df
+dfnew =df.sort_values(by=['num_comments','com_len'], ascending=[False,True])
+
+
+#print top two records from df
+print(dfnew[['title','num_comments','com_len']].head(2))
