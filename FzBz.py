8de4da83965a723c05078b943bf2393307507d06
paw2py <pawanrudgi@gmail.com>
2021-06-03 08:32:29 -0500

    hot fix before release

--- FzBz.py
+++ FzBz.py
@@ -21,3 +21,5 @@
 print(fizzBuzzz(15)
 Output:
       ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz","pawan","rudgi","aol"]
+      
+      'hot fixing code now, this will go before Release branch'
